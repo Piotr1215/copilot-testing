f339b39d0ff3c15c88320f257da9451ab438801a
decoder <decoder@lilve.de>
2021-07-23 23:11:14 +0200

    twofer and converter

---
+++ go/converter.go
@@ -0,0 +1,14 @@
+// Create command line tool to convert grams to kilograms.
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var weight float64
+	fmt.Print("Enter weight in grams: ")
+	fmt.Scanf("%f", &weight)
+	kg := weight / 1000
+	fmt.Printf("%.2f grams is %.2f kilograms\n", weight, kg)
+}
