ea7471e369355a1fdab65bff2c16b980be7717ae
decoder <decoder@live.de>
2021-08-25 23:00:19 +0200

    game

---
+++ game/main.go
@@ -0,0 +1,51 @@
+// create a simple game where noone can win
+//
+//
+//
+//
+//
+//
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a int = 0
+	var b int = 0
+	var c int = 0
+	var d int = 0
+	var e int = 0
+	var f int = 0
+	var g int = 0
+	var h int = 0
+	var i int = 0
+	var j int = 0
+	var k int = 0
+	var l int = 0
+	var m int = 0
+	var n int = 0
+	var o int = 0
+	var p int = 0
+	var q int = 0
+	var r int = 0
+	var s int = 0
+	var t int = 0
+	var u int = 0
+	var v int = 0
+	var w int = 0
+	var x int = 0
+	var y int = 0
+	var z int = 0
+
+	for {
+		fmt.Println("a:", a, "b:", b, "c:", c, "d:", d, "e:", e, "f:", f, "g:", g, "h:", h, "i:", i, "j:", j, "k:", k, "l:", l, "m:", m, "n:", n, "o:", o, "p:", p, "q:", q, "r:", r, "s:", s, "t:", t, "u:", u, "v:", v, "w:", w, "x:", x, "y:", y, "z:", z)
+		fmt.Println("Enter a letter to guess:")
+		var input string
+		fmt.Scanln(&input)
+		fmt.Println(input)
+
+	}
+}
